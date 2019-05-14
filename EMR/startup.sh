6fe7fd479390c55a9518af2a73c2842b3eb672b3
lucananni93 <lucananni93@gmail.com>
2019-05-14 11:40:01 +0200

    added EMR

---
+++ EMR/startup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Installing miniconda"
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O /home/hadoop/miniconda.sh \
+    && /bin/bash ~/miniconda.sh -b -p $HOME/miniconda
+
+echo -e '\nexport PATH=$HOME/miniconda/bin:$PATH' >> $HOME/.bashrc && source $HOME/.bashrc
+rm ~/miniconda.sh
+
+conda init bash
+
+sudo yum install -y tmux
+sudo yum install -y git
+
+echo "Installing python dependencies"
+conda activate 
+pip install git+https://github.com/DEIB-GECO/PyGMQL
